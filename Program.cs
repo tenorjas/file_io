0ebd28ea4cf2cb246de8400437aca7f4c6b1b4a2
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-18 10:14:57 -0400

    created five new car variables in Program class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace file_io
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var corolla = new Car() { ModelYear = 2015, Make = "Toyota", Model = "Corolla", Doors = 4, Miles = 23595 };
+
+            var civic = new Car() { ModelYear = 2004, Make = "Honda", Model = "Civic", Doors = 4, Miles = 186274 };
+
+            var camaro = new Car() { ModelYear = 2010, Make = "Chevrolet", Model = "Camaro", Doors = 2, Miles = 96837 };
+
+            var altima = new Car() { ModelYear = 2014, Make = "Nissan", Model = "Altima", Doors = 4, Miles = 36489 };
+
+            var fusion = new Car() { ModelYear = 2017, Make = "Ford", Model = "Fusion", Doors = 4, Miles = 1024 };
+
         }
     }
 }
